4b575fc0904a14bb45cf45e9b7a036dcd4ae3bad
Nike <Nike@Nikes-Mac-Pro.local>
2015-05-30 01:16:34 +0600

    band image view

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,6 @@ class LandingController < ApplicationController
   def welcome
   	@youtube = YoutubeEmbed.where(:active => true).last || YoutubeEmbed.last
   	@release = Release.where(:active => true).last || Release.last
+  	@frontpage = Frontpage.where(:active => true).last || Frontpage.last
   end
 end
