cf2548f47496e7ccca66abd80a0afb2c80a4805f
Максим Кузьмин <maximkuzmin>
2015-06-03 09:33:55 +0600

    secret files moved to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:maximkuzmin/broken-air.git'
 
 
 set :deploy_to, '/home/deploy/broken-air'
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml, config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default branch is :master
