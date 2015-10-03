81083c5f7d1ae756d74276e9446f3c73ef2751f8
maximkuzmin <photographer.max@gmail.com>
2015-10-04 04:19:43 +0600

    dinamic gig adding added

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,5 +3,6 @@ class LandingController < ApplicationController
   	@youtube = YoutubeEmbed.where(:active => true).last || YoutubeEmbed.last
   	@release = Release.where(:active => true).last || Release.last
   	@frontpage = Frontpage.where(:active => true).last || Frontpage.last
+    @gig = Gig.actual
   end
 end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,5 +1,7 @@
 class Gig < ActiveRecord::Base
   has_attached_file :poster, styles: { medium: "2000x2000>" }
   validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :poster
 
+  scope :actual, -> { where("event_date > :date", :date =>  Date.today).order("event_date ASC").first }
 end
