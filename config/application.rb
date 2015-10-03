5ab49bd1dd9c08f6572cedefec9f1d2d9dd6e6e9
maximkuzmin <photographer.max@gmail.com>
2015-10-04 04:50:26 +0600

    gig data localized, rails admin localized

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Air
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ru
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.paths << "#{Rails.root}/app/assets/frontpages"
