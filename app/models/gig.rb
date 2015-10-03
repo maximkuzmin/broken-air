13974a71edc1bcae1857397532b2bd2165f10f05
maximkuzmin <photographer.max@gmail.com>
2015-10-04 04:29:52 +0600

    dinamic gig adding added

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,7 +1,11 @@
 class Gig < ActiveRecord::Base
+  attr_accessor :actual
   has_attached_file :poster, styles: { medium: "2000x2000>" }
   validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
   validates_presence_of :poster
 
-  scope :actual, -> { where("event_date > :date", :date =>  Date.today).order("event_date ASC").first }
+  private
+  def self.actual
+    Gig.all.select{|g| g.event_date > Date.today }.sort_by{|g| g.event_date}.first
+  end
 end
